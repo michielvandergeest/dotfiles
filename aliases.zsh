9d32f7076f67ed98409510aa7588d43e30098d09
Michiel van der Geest <hello@michielvandergeest.com>
2018-07-25 15:43:56 -0300

    Initial commit. Very basic .zshrc setup.

---
+++ aliases.zsh
@@ -0,0 +1,5 @@
+# Shortcuts
+weather() { curl -4 wttr.in/${1:-sao_paulo} }
+
+# Git
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
